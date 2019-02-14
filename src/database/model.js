81f8038535692ef1cde2bd2f7eb53e21097b5508
carlosf.s.brown@gmail.com <carlosf.s.x@hotmail.com>
2019-02-14 18:00:53 -0200

    configuração inicial do pouchDB

--- src/database/model.js
+++ src/database/model.js
@@ -1,3 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { PouchDB, Find } from "react-pouchdb";
 
+class conexao{
+	constructor(){
+		const db = new PouchDB('todos');
+		const remoteCouch = "http://172.20.1.96:5984/sftb2b";		
+	}
+
+	
+
+
+
+}
