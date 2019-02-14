3bf4155f232f2792d4eb1c542e6d3a31894628bd
carlosf.s.brown@gmail.com <carlosf.s.x@hotmail.com>
2019-02-14 17:55:31 -0200

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      usuario: '',
+      senha: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+   handleSubmit(event) {
+    alert('Usuário ' + this.state.usuario+"\nSenha: "+this.state.senha);
+    event.preventDefault();
+  }
+
+  handleInputChange(event) {
+    if(event.target.name == "usuario"){
+      this.setState({usuario: event.target.value});  
+    }
+    else if(event.target.name == "senha"){
+      this.setState({senha: event.target.value});  
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Usuário:
+          <input
+            name="usuario"
+            type="text"
+            value={this.state.usuario}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Senha
+          <input
+            name="senha"
+            type="password"
+            value={this.state.senha}
+            onChange={this.handleInputChange} />
+        </label>
+         <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
-
 export default App;
